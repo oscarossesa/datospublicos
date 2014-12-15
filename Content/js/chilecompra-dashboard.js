50818eff673529aec5ec73ede83575425fe0c6f2
Oscar Ignacio Osses Arriaza <oscarossesa@gmail.com>
2014-12-14 22:36:25 -0300

    Contador de licitaciones y ordenes de compra

---
+++ Content/js/chilecompra-dashboard.js
@@ -0,0 +1,266 @@
+﻿$(document).ready(function () {
+    setTimeout("show5()", 1000);
+    mostrarFecha();
+
+    //en minutos
+    var minutos = 1;
+    var intervalo = 1000 * 60 * minutos;
+
+    //en milisegundos
+    //var intervalo = 10000;
+
+    var activo = true;
+
+    if (activo) {
+        cargarIndicadoresLc();
+        cargarIndicadoresOc();
+
+        setInterval(function () {
+            cargarIndicadoresLc();
+            cargarIndicadoresOc();
+        }, intervalo);
+    }
+});
+
+function obtenerFecha() {
+    var f = new Date();
+
+    var dia = f.getDate();
+    var mes = f.getMonth() + 1;
+    var anho = f.getFullYear();
+
+    return (dia < 10 ? '0' : '') + dia + (mes < 10 ? '0' : '') + mes + f.getFullYear();
+}
+
+function mostrarFecha() {
+    var f = new Date();
+
+    var dia = f.getDate();
+    var mes = f.getMonth();
+    var anho = f.getFullYear();
+
+    meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+    $("#spnfecha").text(dia + " " + "de " + meses[mes] + " del " + anho);
+}
+
+function getUrl(objeto, estado) {
+    //var host = "http://localhost/";
+    //var host = "http://desarrollo2:8080/";
+    var host = "http://api.mercadopublico.cl/";
+    var version = "v1";
+    var objeto = objeto;
+    var estado = estado;
+    var fecha = getFecha();
+    //var ticket = "D6DF9A69-2F0E-482E-B1F4-55A234C8193F"; //des
+    var ticket = "78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1"; //pro
+    host + "servicios/" + version + "/publico/" + objeto + "/" + estado + "/" + fecha + "?ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1";
+    var url = host + "servicios/" + version + "/publico/" + objeto + "/" + estado + "/" + fecha + "?ticket=" + ticket;
+    return url;
+}
+
+function cargarIndicadoresLc() {
+
+    var objeto = "licitaciones";
+    var respuesta = "jsonp";
+
+    $.ajax({
+        //url: url,
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=todos&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#TodasLc").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/todos"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "publicada"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=publicada&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Publicadas").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/publicada"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "cerrada"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=cerrada&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Cerradas").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/cerrada"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "desierta"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=desierta&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Desiertas").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/desierta"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "adjudicada"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=adjudicada&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Adjudicadas").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/adjudicada"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "revocada"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=revocada&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Revocadas").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/revocada"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "suspendida"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/licitaciones.jsonp?estado=suspendida&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Suspendidas").text(data.Cantidad);
+        },
+        error: function (data) { console.log("licitacion/suspendida"); }
+    });
+
+}
+
+function cargarIndicadoresOc() {
+    
+    var objeto = "ordencompra";
+
+    $.ajax({
+        //url: getUrl(objeto, "todos"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.jsonp?estado=todos&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#TodasOc").text(data.Cantidad);
+        },
+        error: function (data) { console.log("ordencompra/todos"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "enviadaproveedor"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.jsonp?estado=enviadaproveedor&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#EnviadaaProveedor").text(data.Cantidad);
+        },
+        error: function (data) { console.log("ordencompra/enviadaproveedor"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "aceptada"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.jsonp?estado=aceptada&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Aceptada").text(data.Cantidad);
+        },
+        error: function (data) { console.log("ordencompra/aceptada"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "cancelada"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.jsonp?estado=cancelada&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#Cancelada").text(data.Cantidad);
+        },
+        error: function (data) { console.log("ordencompra/cancelada"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "recepcionconforme"),
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.jsonp?estado=recepcionconforme&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#RecepcionConforme").text(data.Cantidad);
+        },
+        error: function (data) { console.log("ordencompra/recepcionconforme"); }
+    });
+
+    $.ajax({
+        //url: getUrl(objeto, "pendienterecepcion"),}
+        url: "http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.jsonp?estado=pendienterecepcion&ticket=78D4FEE6-7D6B-4EE6-85A8-6B272E237AC1",
+        type: "get",
+        dataType: "jsonp",
+        async: true,
+        success: function (data) {
+            $("#PendientedeRecepcionar").text(data.Cantidad);
+        },
+        error: function (data) { console.log("ordencompra/pendienterecepcion"); }
+    });
+}
+
+function show5() {
+    if (!document.layers && !document.all && !document.getElementById)
+        return
+
+    var Digital = new Date()
+    var hours = Digital.getHours()
+    var minutes = Digital.getMinutes()
+    var seconds = Digital.getSeconds()
+
+    var hours = Digital.getUTCHours();
+    console.log(hours);
+
+    var dn = "PM"
+    if (hours < 12)
+        dn = "AM"
+    if (hours > 12)
+        hours = hours - 12
+    if (hours == 0)
+        hours = 12
+
+    if (minutes <= 9)
+        minutes = "0" + minutes
+    if (seconds <= 9)
+        seconds = "0" + seconds
+    //change font size here to your desire
+    //myclock = "<font size='5' face='Arial' ><b><font size='1'>Hora actual:</font></br>" + hours + ":" + minutes + ":"
+    myclock = "Hora Actual: " + hours + ":" + minutes + ":" + seconds + " " + dn;
+    if (document.layers) {
+        document.layers.liveclock.document.write(myclock)
+        document.layers.liveclock.document.close()
+    }
+    else if (document.all)
+        liveclock.innerHTML = myclock
+    else if (document.getElementById)
+        document.getElementById("liveclock").innerHTML = myclock
+    setTimeout("show5()", 1000)
+}
---
+++ Content/js/chilecompra-graficos.js
@@ -0,0 +1,267 @@
+﻿//licitaciones
+//labels: ["Todas", "Publicadas", "Cerradas", "Desiertas", "Adjudicadas", "Revocadas", "Suspendidas"],
+
+//ordenes de compra
+//labels: ["Todas", "Enviada a Proveedor", "Aceptada", "Cancelada", "Recepción Conforme", "Pendiente de Recepcionar", "Recepcionada Parcialmente", "Recepcion Conforme Incompleta"],
+
+options = {
+    // Boolean - Whether to animate the chart
+    animation: true,
+
+    // Number - Number of animation steps
+    animationSteps: 60,
+
+    // String - Animation easing effect
+    animationEasing: "easeOutQuart",
+
+    // Boolean - If we should show the scale at all
+    showScale: true,
+
+    // Boolean - If we want to override with a hard coded scale
+    scaleOverride: false,
+
+    // ** Required if scaleOverride is true **
+    // Number - The number of steps in a hard coded scale
+    scaleSteps: null,
+    // Number - The value jump in the hard coded scale
+    scaleStepWidth: null,
+    // Number - The scale starting value
+    scaleStartValue: null,
+
+    // String - Colour of the scale line
+    scaleLineColor: "rgba(0,0,0,.1)",
+
+    // Number - Pixel width of the scale line
+    scaleLineWidth: 1,
+
+    // Boolean - Whether to show labels on the scale
+    scaleShowLabels: true,
+
+    // Interpolated JS string - can access value
+    scaleLabel: "<%=value%>",
+
+    // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
+    scaleIntegersOnly: true,
+
+    // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+    scaleBeginAtZero: false,
+
+    // String - Scale label font declaration for the scale label
+    scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+    // Number - Scale label font size in pixels
+    scaleFontSize: 12,
+
+    // String - Scale label font weight style
+    scaleFontStyle: "normal",
+
+    // String - Scale label font colour
+    scaleFontColor: "#666",
+
+    // Boolean - whether or not the chart should be responsive and resize when the browser does.
+    responsive: false,
+
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio: true,
+
+    // Boolean - Determines whether to draw tooltips on the canvas or not
+    showTooltips: true,
+
+    // Array - Array of string names to attach tooltip events
+    tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+
+    // String - Tooltip background colour
+    tooltipFillColor: "rgba(0,0,0,0.8)",
+
+    // String - Tooltip label font declaration for the scale label
+    tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+    // Number - Tooltip label font size in pixels
+    tooltipFontSize: 14,
+
+    // String - Tooltip font weight style
+    tooltipFontStyle: "normal",
+
+    // String - Tooltip label font colour
+    tooltipFontColor: "#fff",
+
+    // String - Tooltip title font declaration for the scale label
+    tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+
+    // Number - Tooltip title font size in pixels
+    tooltipTitleFontSize: 14,
+
+    // String - Tooltip title font weight style
+    tooltipTitleFontStyle: "bold",
+
+    // String - Tooltip title font colour
+    tooltipTitleFontColor: "#fff",
+
+    // Number - pixel width of padding around tooltip text
+    tooltipYPadding: 6,
+
+    // Number - pixel width of padding around tooltip text
+    tooltipXPadding: 6,
+
+    // Number - Size of the caret on the tooltip
+    tooltipCaretSize: 8,
+
+    // Number - Pixel radius of the tooltip border
+    tooltipCornerRadius: 6,
+
+    // Number - Pixel offset from point x to tooltip edge
+    tooltipXOffset: 10,
+
+    // String - Template string for single tooltips
+    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+
+    // String - Template string for single tooltips
+    multiTooltipTemplate: "<%= value %>",
+
+    // Function - Will fire on animation progression.
+    onAnimationProgress: function () { },
+
+    // Function - Will fire on animation completion.
+    onAnimationComplete: function () { }
+}
+
+$(document).ready(function () {
+
+    var dataLc = [
+        {
+            value: 0,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 0,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 4,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 27,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        },
+        {
+            value: 0,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 0,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        }
+    ];
+
+    var dataOc = [
+        {
+            value: 3,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 185,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 1,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 47,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        },
+        {
+            value: 0,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 0,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 0,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        }
+    ];
+
+    new Chart(document.getElementById("pie-chart-lc").getContext("2d")).Pie(dataLc);
+    new Chart(document.getElementById("pie-chart-oc").getContext("2d")).PolarArea(dataOc);
+
+    var dataBarraLc = {
+        labels: ["T", "P", "C", "D", "A", "R", "S"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [31, 0, 0, 4, 27, 0, 0]
+            },
+        ]
+    };
+
+    new Chart(document.getElementById("bar-chart-lc").getContext("2d")).Bar(dataBarraLc, options);
+
+    var dataBarraOc = {
+        labels: ["T", "EP", "A", "C", "RC", "PR", "RP", "RCI"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [0, 3, 185, 1, 47, 0, 0, 0]
+            },
+        ]
+    };
+
+    new Chart(document.getElementById("bar-chart-oc").getContext("2d")).Bar(dataBarraOc);
+
+    var lineChartData = {
+        labels: ["T", "P", "C", "D", "A", "R", "S"],
+        datasets: [
+			{
+			    fillColor: "rgba(151,187,205,0.5)",
+			    strokeColor: "rgba(151,187,205,1)",
+			    pointColor: "rgba(151,187,205,1)",
+			    pointStrokeColor: "#fff",
+			    data: [28, 48, 40, 19, 96, 27, 100]
+			}
+		]
+    }
+
+    //new Chart(document.getElementById("line-chart-lc").getContext("2d")).Line(lineChartData);
+    var ctx = document.getElementById("line-chart-lc").getContext("2d");
+    var myLineChart = new Chart(ctx).Line(lineChartData, options);
+
+});
+
